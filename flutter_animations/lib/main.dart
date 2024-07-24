afe97c0dbdb13c611f6ecba19afab3da814b0ff9
azix khan <azixkhan.55@gmail.com>
2024-07-24 16:43:56 +0500

    called in main.dart

--- flutter_animations/lib/main.dart
+++ flutter_animations/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animations/screens/home.dart';
+import 'package:flutter_animations/screens/sandbox.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Animations Trips',
-      home: Home(),
+      home: Sandbox(),
     );
   }
 }
