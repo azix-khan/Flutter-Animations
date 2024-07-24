59babe514226ed637f61f9f1447c3c1961c3af17
azix khan <azixkhan.55@gmail.com>
2024-07-24 16:29:07 +0500

    setting up project for flutter animations

---
+++ flutter_animations/lib/screens/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animations/shared/screenTitle.dart';
+import 'package:flutter_animations/shared/tripList.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/bg.png"),
+                  fit: BoxFit.fitWidth,
+                  alignment: Alignment.topLeft),
+            ),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 30),
+                SizedBox(
+                  height: 160,
+                  child: ScreenTitle(
+                    text: 'Ninja Trips',
+                  ),
+                ),
+                Flexible(
+                  child: TripList(),
+                )
+                //Sandbox(),
+              ],
+            )));
+  }
+}
